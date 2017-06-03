c57388f0dd86f2aa12e13bda6b6f5a16bb888616
vlajko <vlajko.dragicevic@gmail.com>
2017-06-03 19:27:29 +0200

    add gui

--- src/App.java
+++ src/App.java
@@ -1,31 +1,34 @@
-import models.Smer;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import services.DosijeService;
-import services.IspitService;
-import services.PredmetService;
-import services.SmerService;
-import utils.HibernateUtil;
-
-import java.util.List;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
 /**
  * Created by vlajko on 5/30/17.
  */
-public class App {
+public class App extends Application {
+
+    private Button button;
 
     public static void main(String args[]) {
+        launch(args);
         System.out.println("Start App Here");
-
-//        SmerService.readSmer();
-//        IspitService.readAllIspit();
-//        PredmetService.readPredmet();
-        DosijeService.readDosije();
-        HibernateUtil.getSessionFactory().close();
-        System.out.println("Kraj");
-
-
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Semirnarski");
+
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
---
+++ src/gui/Gui.java
@@ -0,0 +1,7 @@
+package gui;
+
+/**
+ * Created by vlajko on 6/3/17.
+ */
+public class Gui {
+}
